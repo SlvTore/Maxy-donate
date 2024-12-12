d21b78b62a7bedf4a6300c9f28a593f1d4cf0381
DitaAD <ditaannisa2004@gmail.com>
2024-12-12 09:48:40 +0700

    again

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,6 @@ import AboutUs from './pages/tentangkami.jsx';
 import RegisterPage from './pages/regist.jsx';
 import Pembayaran from './pages/Pembayaran.jsx';
 import Anggota from './components/User/Anggota/data_anggota.jsx';
-
-import Anggota from './components/User/Anggota/data_anggota.jsx';
-import Anggota from './components/User/Anggota/data_anggota.jsx';
 import Program from './pages/program.jsx';
 
 
@@ -32,9 +29,7 @@ function App() {
         <Route path='/aboutus' element={<><Nav /><AboutUs /></>} />
         <Route path='/regist' element={<><Nav /><RegisterPage /></>} />
         <Route path='/Pembayaran' element={<><Pembayaran /></>} />
-        <Route path='/data_anggota' element={<><Navigation /><Anggota /></>} />
         <Route path="/konfirmasi-pembayaran" element={<KonfirmasiPembayaran />} />
-        <Route path='/data_anggota' element={<><Navigation /><Anggota /></>} />
         <Route path='/data_anggota' element={<><Navigation /><Anggota /></>} />
         <Route path='/program' element={<><Navigation /><Program /></>} />
       </Routes>
